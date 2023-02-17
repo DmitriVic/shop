3c6d813db4a87559d2786b6bc96351f1155f19aa
DmitriVic <Mr_vlasov@mail.ru>
2023-02-17 21:36:35 +0300

    поправил поля формы

--- src/components/Registration/FormReg/index.tsx
+++ src/components/Registration/FormReg/index.tsx
@@ -1,10 +1,11 @@
- import s from './index.module.css'
+import s from "./index.module.css";
 // import { ButtonProps } from './Button.props'
 // import cn from 'classnames'
 // import ArrowIcon from './arrow.svg';
 
 import { useForm } from "react-hook-form";
 import { indexProps } from "./index.props";
+import { useState } from "react";
 
 // export const Button = ({ appearance, arrow = 'none',  children, className, ...props }: ButtonProps): JSX.Element => {
 // 	return (
@@ -24,33 +25,47 @@ import { indexProps } from "./index.props";
 // 	</button>)
 // }
 
-export const FormReg = ({setChangeModal}: indexProps): JSX.Element => {
+export const FormReg = ({ setChangeModal }: indexProps): JSX.Element => {
+  const [isPasswordVisible, setPasswordVisibility] = useState(false);
   const {
     register,
     handleSubmit,
-    formState: { errors }, } = useForm();
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = (data: any) => console.log(data);
-  
-  console.log(errors);
 
+  console.log(errors);
+  const togglePasswordVisibility = () => {
+    setPasswordVisibility((a) => !a);
+  };
 
   return (
     <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
-		<h2>Зарегистрироваться</h2>
+      <h2>Создать акаунт</h2>
       <input
         type="text"
         placeholder="First name"
         {...register("First name", { required: true, maxLength: 80 })}
       />
-      <input
-        type="text"
-        placeholder="Email"
-        {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
-      />
 
-      <button>Регистрация</button>
-		<div onClick={()=>setChangeModal(a =>!a)}>Войти</div>
+      <label htmlFor="">
+        <input
+          type={isPasswordVisible ? "text" : "password"}
+          placeholder="Password"
+          {...register("Password", { required: true, maxLength: 12 })}
+        />
+        <span className={s["hidden-pass"]} onClick={togglePasswordVisibility}>
+          скрыть пароль
+        </span>
+      </label>
+		<button>Отправить</button>
+		<div
+        onClick={() => setChangeModal((a) => !a)}
+        className={s["switch-modal"]}
+      >
+        Вход
+      </div>
     </form>
   );
 };
