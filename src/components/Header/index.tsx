e14a2b83e3881bb96272c68cc6f20bd6b9c593c0
DmitriVic <Mr_vlasov@mail.ru>
2023-02-17 14:23:26 +0300

    роутинг модального окна без реакт-роутер

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -28,7 +28,7 @@ import { indexProps } from "./index.props"
 export const Header = ({  }:indexProps): JSX.Element => {
 	return (
 		<div className={s.header}>
-			<div>Ветка DEV</div>
+			<div className={s.branch}>Ветка DEV</div>
 			<h1>header</h1>
 			<Registration/>
 		</div>
