b9bacd209ff73b7fb26037c6bca08ad94576782d
DmitriVic <Mr_vlasov@mail.ru>
2023-02-18 13:31:05 +0300

    обновил

--- src/components/Registration/index.tsx
+++ src/components/Registration/index.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import { FormAufh } from "./FormAufh";
-import { FormReg } from "./FormReg";
-//import { Modal } from "../Modal/Modal";
 import s from "./index.module.css";
 // import { ButtonProps } from './Button.props'
 // import cn from 'classnames'
@@ -10,41 +8,15 @@ import s from "./index.module.css";
 import { indexProps } from "./index.props";
 import { Modal } from "./Modal/Modal";
 
-// export const Button = ({ appearance, arrow = 'none',  children, className, ...props }: ButtonProps): JSX.Element => {
-// 	return (
-// 	<button  className={cn(s.button, className, {
-// 		[s.primary]: appearance == 'primary',
-// 		[s.ghost]: appearance == 'ghost',
-// 	})}
-// 	{...props}
-// 	>
-// 		{children}
-// 		{arrow != 'none' && <span className={cn(s.arrow, {
-// 				[s.down]: arrow == 'down'
-// 			})}>
-// 				<ArrowIcon />
-// 			</span>}
-
-// 	</button>)
-// }
-
 export const Registration = ({}: indexProps): JSX.Element => {
   const [modalActive, setModalActive] = useState(false);
-  //const [changeModal, setChangeModal] = useState(true);
-  //console.log(setChangeModal);
   return (
     <div>
       <div className={s.registration} onClick={() => setModalActive(true)}>
         войти
       </div>
-      <Modal
-		 modalActive={modalActive} 
-		 setModalActive={setModalActive}>
-			
-        <FormAufh 
-		  //setChangeModal={setChangeModal} 
-		  //changeModal={changeModal}
-		   />
+      <Modal modalActive={modalActive} setModalActive={setModalActive}>
+        <FormAufh />
       </Modal>
     </div>
   );
