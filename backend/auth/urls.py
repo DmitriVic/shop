99eaef61d18094cafd8fae5b78aacadd44cfccae
Zaycev Andrey <mr_zaycev@mail.ru>
2023-02-19 21:55:54 +0300

    добавил JWT

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views import UserViewSet
 
@@ -10,5 +11,7 @@ router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
