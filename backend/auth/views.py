50c15767b906adb8a7e22fd9e6f24fd307c0ba90
Dronza79 <113519920+Dronza79@users.noreply.github.com>
2023-02-20 11:47:17 +0300

    Update views.py
    откорректировал доступность список доступен всем, детальная информация только зарегистрированным

--- backend/auth/views.py
+++ backend/auth/views.py
@@ -1,9 +1,22 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets
+from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 
 from .serializers import UserSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class UserListViewSet(mixins.CreateModelMixin,
+                      mixins.ListModelMixin,
+                      GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
