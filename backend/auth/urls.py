55ca523dca5cb926bb90f3d651448c960814fe57
Dronza79 <113519920+Dronza79@users.noreply.github.com>
2023-02-20 11:46:40 +0300

    Update urls.py
    откорректировал доступность список доступен всем, детальная информация только зарегистрированным

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -3,15 +3,17 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import UserViewSet
+from .views import UserViewSet, UserListViewSet
 
-router = routers.DefaultRouter()
-router.register(r'api/users', UserViewSet)
+router1 = routers.DefaultRouter()
+router1.register(r'detail', UserViewSet)
 
+router2 = routers.DefaultRouter()
+router2.register(r'api', UserListViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/users/', include(router1.urls)),
+    path('', include(router2.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
