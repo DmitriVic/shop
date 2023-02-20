a662a96b652d7fed866f09f60524153bba9e085b
Dronza79 <113519920+Dronza79@users.noreply.github.com>
2023-02-20 13:11:52 +0300

    Update urls.py
    рефакторинг

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -5,15 +5,16 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views import UserViewSet, UserListViewSet
 
-router1 = routers.DefaultRouter()
-router1.register(r'detail', UserViewSet)
+router = routers.DefaultRouter()
+router.register(r'api/auth', UserListViewSet)
 
-router2 = routers.DefaultRouter()
-router2.register(r'api', UserListViewSet)
+router_detail = routers.DefaultRouter()
+router_detail.register('', UserViewSet)
+
 
 urlpatterns = [
-    path('api/users/', include(router1.urls)),
-    path('', include(router2.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
+    path('api/auth/users/', include(router_detail.urls)),
+    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
