ad3620746736a2629bfb85dd08e040ea78ee7def
Dronza79 <113519920+Dronza79@users.noreply.github.com>
2023-02-20 09:11:50 +0300

    Update serializers.py
    Добавил возможность сохранения пользователя в БД с хешем пароля а не с открытым паролем

--- backend/auth/serializers.py
+++ backend/auth/serializers.py
@@ -6,3 +6,19 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'password', 'is_staff']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'is_staff': {'write_only': True},
+        }
+    
+    def create(self, validated_data):
+        if "password" in validated_data:
+            from django.contrib.auth.hashers import make_password
+            validated_data["password"] = make_password(validated_data["password"])
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        if "password" in validated_data:
+            from django.contrib.auth.hashers import make_password
+            validated_data["password"] = make_password(validated_data["password"])
+        return super().update(instance, validated_data)
