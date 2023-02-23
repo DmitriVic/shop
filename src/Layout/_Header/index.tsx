1378d78f6aa0286dd995133a0cb0309c6a2a3d61
Dmitiy <mr_vlasov@mail.ru>
2023-02-23 15:37:48 +0300

    create branch loayout

---
+++ src/Layout/_Header/index.tsx
@@ -0,0 +1,36 @@
+ import s from './index.module.css'
+// import { ButtonProps } from './Button.props'
+// import cn from 'classnames'
+// import ArrowIcon from './arrow.svg';
+
+import { indexProps } from "./index.props"
+
+// export const Button = ({ appearance, arrow = 'none',  children, className, ...props }: ButtonProps): JSX.Element => {
+// 	return (
+// 	<button  className={cn(s.button, className, {
+// 		[s.primary]: appearance == 'primary',
+// 		[s.ghost]: appearance == 'ghost',
+// 	})}
+// 	{...props}
+// 	>
+// 		{children}
+// 		{arrow != 'none' && <span className={cn(s.arrow, {
+// 				[s.down]: arrow == 'down'
+// 			})}>
+// 				<ArrowIcon />
+// 			</span>}
+			
+// 	</button>)
+// }
+
+
+export const _Header = ({ className }:indexProps): JSX.Element => {
+	return (
+		<div className={className}>
+			<div className={s.wrapper}>
+				<h1>Брошки от Антошки</h1>
+			</div>
+		</div>
+	)
+}
+
---
+++ src/Layout/index.tsx
@@ -0,0 +1,46 @@
+import s from './index.module.css'
+// import { ButtonProps } from './Button.props'
+// import cn from 'classnames'
+// import ArrowIcon from './arrow.svg';
+
+import { Footer } from "./Footer"
+import { _Main } from "./_Main"
+import { _Header } from "./_Header"
+import { indexProps } from "./index.props"
+
+// export const Button = ({ appearance, arrow = 'none',  children, className, ...props }: ButtonProps): JSX.Element => {
+// 	return (
+// 	<button  className={cn(s.button, className, {
+// 		[s.primary]: appearance == 'primary',
+// 		[s.ghost]: appearance == 'ghost',
+// 	})}
+// 	{...props}
+// 	>
+// 		{children}
+// 		{arrow != 'none' && <span className={cn(s.arrow, {
+// 				[s.down]: arrow == 'down'
+// 			})}>
+// 				<ArrowIcon />
+// 			</span>}
+			
+// 	</button>)
+// }
+
+
+export const Layout = ({  }:indexProps): JSX.Element => {
+	return (
+		<div className={s.wrapper}>	
+			<_Header
+			className={s.header}
+			/>
+			<_Main
+			className={s.main}>
+
+			</_Main>
+			<Footer
+			className={s.footer}
+			/>
+		</div>
+	)
+}
+
