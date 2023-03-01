4c122277aa6d511e2ebfc7cd5cfac3361dd404ba
Dmitiy <mr_vlasov@mail.ru>
2023-03-01 13:37:37 +0300

    набросок ЛК

--- src/components/FormEdit/index.tsx
+++ src/components/FormEdit/index.tsx
@@ -1,23 +1,20 @@
 import s from "./index.module.css";
 // import { ButtonProps } from './Button.props'
-// import cn from 'classnames'
+import cn from 'classnames'
 // import ArrowIcon from './arrow.svg';
 
 import { SubmitHandler, useForm } from "react-hook-form";
 import { indexProps } from "./index.props";
 
 interface IFormInput {
-	email: string
-first_name: string
-second_name:string
-last_name:string
-phonenumber:number
-zip_code:number
-delivery_address:string
- }
- 
-
-
+  email: string;
+  first_name: string;
+  second_name: string;
+  last_name: string;
+  phonenumber: number;
+  zip_code: number;
+  delivery_address: string;
+}
 
 export const FormEdit = ({}: indexProps): JSX.Element => {
   const {
@@ -25,73 +22,116 @@ export const FormEdit = ({}: indexProps): JSX.Element => {
     handleSubmit,
     formState: { errors },
   } = useForm<IFormInput>();
-  const onSubmit: SubmitHandler<IFormInput> = data => console.log(data);
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
 
   //console.log(errors);
-  console.log(errors.phonenumber);
+  //console.log(errors.phonenumber);
   return (
     <div className={s.container}>
       <form className={s["form-auth"]} onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="">
+
+
+        <div className={s["person-date"]}>
+		  <div className={s['title']}>Персональные данные</div>
+
+          <div className={s["person-date__box"]}>
+            <div className={s["avatar"]}>
+              <img src="" alt="" />
+            </div>
+
+            <div className={s['initials']}>
+					<label  htmlFor="">
+					  <input
+					  	id="first_name"
+						 className={s["inpt"]}
+						 type="text"
+						 placeholder="Имя"
+						 {...register("first_name", {})}
+					  />
+					</label>
+					<input
+					  className={s["inpt"]}
+					  type="text"
+					  placeholder="Отчество"
+					  {...register("second_name", {})}
+					/>
+					<input
+					  className={s["inpt"]}
+					  type="text"
+					  placeholder="Фамилия"
+					  {...register("last_name", {})}
+					/>
+				</div>
+          </div>
+
+
+          <input placeholder="Дата рождения" className={cn(s["date-birth"],s["inpt"])} type="text" />
+
+          <div className={s['buttons']}>
+				<button type="submit" className={s["btn1"]}>
+				  Сохранить
+				</button>
+				<button className={s["btn1"]}>Выйти</button>
+			 </div>
+         
+        </div>
+
+        <div className={s["data-communication"]}>
+		  <div className={s['title']}>Данные для связи</div>
+          <div>
+				<label htmlFor="">
+				  <input
+					 className={s["inpt"]}
+					 type="tel"
+					 placeholder="Телефонный номер"
+					 {...register("phonenumber", {
+						pattern: /^\+?[78][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/,
+					 })}
+				  />
+				  {errors.phonenumber && (
+					 <p className={s["errors"]}>Введите корректно номер телефона</p>
+				  )}
+				</label>
+				<label htmlFor="">
+				  <input
+					 className={s["inpt"]}
+					 type="email"
+					 placeholder="Почта"
+					 {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+				  />
+				  {errors.email?.type === "pattern" && (
+					 <p className={s["errors"]} role="alert">
+						Введите корректный адрес электронной почты
+					 </p>
+				  )}
+				</label>
+			 </div>
+        </div>
+
+        <div className={s["delivery-address"]}>
+			<div className={s['title']}>Адрес доставки</div>
           <input
             className={s["inpt"]}
-            type="email"
-            placeholder="Почта"
-            {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+            type="number"
+            placeholder="Индекс"
+            {...register("zip_code", {})}
           />
-          {errors.email?.type === "pattern" && (
-            <p className={s["errors"]} role="alert">
-              Введите корректный адрес электронной почты
-            </p>
-          )}
-        </label>
 
-        <label htmlFor="">
+          <input
+            type="text"
+            className={s["inpt"]}
+            placeholder="Город"
+            name=""
+            id=""
+          />
+
           <input
             className={s["inpt"]}
             type="text"
-            placeholder="Имя"
-            {...register("first_name", { })}
+            placeholder="Адрес"
+            {...register("delivery_address", {})}
           />
-          
-        </label>
-
-        <input
-          className={s["inpt"]}
-          type="text"
-          placeholder="Отчество"
-          {...register("second_name", {})}
-        />
-
-        <input
-          className={s["inpt"]}
-          type="text"
-          placeholder="Фамилия"
-          {...register("last_name", {})}
-        />
-
-        <input
-          className={s["inpt"]}
-          type="tel"
-          placeholder="Телефонный номер"
-          {...register("phonenumber", { pattern: /^\+?[78][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/ } )}
-        />
-				{errors.phonenumber &&  <p className={s["errors"]}>Введите корректно номер телефона</p> }
-        <input
-          className={s["inpt"]}
-          type="number"
-          placeholder="Индекс"
-          {...register("zip_code", {})}
-        />
-
-        <input
-          className={s["inpt"]}
-          type="text"
-          placeholder="Адрес"
-          {...register("delivery_address", {})}
-        />
-
-        <button className={s["btn1"]}>Сохранить</button>
+        </div>
       </form>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import { Layout } from "./Layout";
 import { Authorization } from "./Pages/Authorization";
 import { Home } from "./Pages/Home";
 import { Registration } from "./Pages/Registration";
 import { Account } from "./Pages/Account";
+import { Notfoundpage } from "./Pages/Notfoundpage";
 
-
-const router = createBrowserRouter(createRoutesFromElements(
-	<Route path="/" element={<Layout/>}>
-				<Route index element={<Home/>}/>
-				<Route path="authorization" element={<Authorization/>}/>
-				<Route path="registration" element={<Registration/>}/>
-				<Route path="account" element={<Account/>}/>
-	</Route>
-))
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="authorization" element={<Authorization />} />
+      <Route path="registration" element={<Registration />} />
+      <Route path="account" element={<Account />}>
+        <Route path="contacts" element={<p>Our contact</p>} />
+        <Route path="team" element={<p>Our team</p>} />
+      </Route>
+		<Route path="*" element={<Notfoundpage />} />
+    </Route>
+	 
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-   <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
